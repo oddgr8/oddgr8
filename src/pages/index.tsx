c3860ea3110bf43648a576884eebc370f27181f4
Onkar Deshpande <onkardeshpande07@gmail.com>
2023-06-12 18:39:11 +0530

    Add contact icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,14 @@ import { type NextPage } from "next";
 import Image from "next/image";
 import me from "../../public/me.png";
 import { titleFont } from "~/shared/fonts";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Musings: NextPage = () => {
   return (
@@ -25,7 +33,7 @@ const Musings: NextPage = () => {
         </p>
       </div>
       <div className="flex flex-col place-items-center">
-        <div className="z-20 m-2 mb-5 h-fit w-fit overflow-hidden rounded-full p-0">
+        <div className="z-20  m-5 h-fit w-fit overflow-hidden rounded-full p-0">
           <Image
             src={me}
             alt="A picture of me"
@@ -35,7 +43,36 @@ const Musings: NextPage = () => {
             placeholder="blur"
           />
         </div>
-        <div className="text-4xl">Something Huge</div>
+        <div className="flex flex-row">
+          {[
+            {
+              path: "https://linkedin.com/in/onkardeshpande07",
+              icon: faLinkedinIn,
+            },
+            {
+              path: "https://twitter.com/oddgr8",
+              icon: faTwitter,
+            },
+            {
+              path: "https://github.com/oddgr8",
+              icon: faGithub,
+            },
+            {
+              path: "mailto:onkardeshpande07@gmail.com",
+              icon: faEnvelope,
+            },
+          ].map((ele, idx) => (
+            <Link
+              className="m-2 mx-5 hover:text-main"
+              href={ele.path}
+              key={idx}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={ele.icon} size="xl" />
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
