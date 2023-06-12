501ce0e06f2e1ca67e6fb1b268c37e3d626f16f1
Onkar Deshpande <onkardeshpande07@gmail.com>
2023-06-12 23:21:33 +0530

    Further refactoring

--- src/shared/layout.tsx
+++ src/shared/layout.tsx
@@ -9,46 +9,64 @@ import { type PropsWithChildren } from "react";
 import { commonFont, titleFont } from "./fonts";
 import { isAuthorized } from "./users";
 
-function Navbar() {
+function Themer() {
   const { resolvedTheme, setTheme } = useTheme();
+  return (
+    <Expand
+      duration={500}
+      toggled={resolvedTheme === "dark"}
+      onToggle={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      className="text-4xl"
+    />
+  );
+}
+
+function Navbar({
+  links,
+  priv,
+  withThemer,
+}: {
+  links: { text: string; subroot: string }[];
+  priv?: boolean;
+  withThemer?: boolean;
+}) {
   const { asPath } = useRouter();
+  const { status, data } = useSession();
+  if (priv)
+    if (status !== "authenticated" || !isAuthorized(data.user.email ?? ""))
+      return null;
   const page = asPath.split("/")[1];
   return (
     <nav
       className={
-        "flex w-full flex-row justify-between p-2 text-acc-dark dark:text-acc-light md:p-5 md:px-10 " +
+        "flex w-full flex-row justify-between p-2 text-acc-dark dark:text-acc-light md:px-10 md:pt-5 " +
         titleFont
       }
     >
       <div className="flex flex-row gap-4 text-4xl">
-        {[
-          { text: "Home", path: "" },
-          { text: "Musings", path: "musings" },
-        ].map(({ text, path }) => (
-          <Link key={text} href={"/" + path} className="m-2">
-            <div className={page == path ? "text-main" : ""}>{text}</div>
+        {links.map(({ text, subroot }) => (
+          <Link key={text} href={"/" + subroot} className="m-2">
+            <div className={page == subroot ? "text-main" : ""}>{text}</div>
           </Link>
         ))}
       </div>
-      <Expand
-        duration={500}
-        toggled={resolvedTheme === "dark"}
-        onToggle={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-        className="text-4xl"
-      />
+      {withThemer && <Themer />}
     </nav>
   );
 }
 
 export default function Layout({ children }: PropsWithChildren) {
-  const { status, data } = useSession();
   return (
     <div className={"flex w-full flex-col place-items-center" + commonFont}>
       <div className="flex h-screen w-full max-w-screen-2xl flex-col items-center">
-        <Navbar />
-        {status === "authenticated" && isAuthorized(data.user.email ?? "") && (
-          <nav>Private</nav>
-        )}
+        <Navbar
+          withThemer
+          links={[
+            { text: "Home", subroot: "" },
+            { text: "Musings", subroot: "musings" },
+          ]}
+        />
+        <Navbar priv links={[{ text: "Expenses", subroot: "expenses" }]} />
         <main className="flex w-full flex-col place-items-center">
           {children}
         </main>
