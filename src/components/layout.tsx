3532d41df40c175bc02e722f45437042267b3d5d
Onkar Deshpande <onkardeshpande07@gmail.com>
2023-06-12 13:40:06 +0530

    Fix navbar links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,35 @@
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
 import { type PropsWithChildren } from "react";
+import { Caveat } from "next/font/google";
+import { useRouter } from "next/router";
+
+const commonFont = Caveat({ subsets: ["latin"] });
 
 export default function Layout({ children }: PropsWithChildren) {
   const { status } = useSession();
+  const { asPath } = useRouter();
+  const page = asPath.split("/")[1];
   return (
-    <div className="flex h-screen flex-col place-items-center ">
-      <nav className="m-5 flex w-full max-w-screen-xl flex-row justify-between">
-        <Link href="/">
-          <Image src="/OD text.png" width="100" height="100" alt="Home" />
-        </Link>
-        <Link href="/musings">
-          <Image src="/Bubble.png" width="100" height="100" alt="Musings" />
-        </Link>
+    <div
+      className={
+        "flex h-screen flex-col place-items-center " + commonFont.className
+      }
+    >
+      <nav className="m-5 flex w-full max-w-screen-lg flex-row justify-between">
+        <div className="flex flex-row gap-4 text-4xl text-acc-dark dark:text-acc-light">
+          {[
+            { text: "Home", path: "" },
+            { text: "Musings", path: "musings" },
+            { text: "Projects", path: "projects" },
+          ].map(({ text, path }) => (
+            <Link key={text} href={"/" + path} className="m-2">
+              <div className={page == path ? "text-main" : ""}>{text}</div>
+            </Link>
+          ))}
+        </div>
+
+        <div>Theme</div>
       </nav>
       {status === "authenticated" && <nav>Private</nav>}
       <main>{children}</main>
